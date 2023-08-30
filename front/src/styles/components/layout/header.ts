3d99d6193f89bce507098c6676596f25d9699e3d
juhee kang <mnmn092631@gmail.com>
2023-08-30 11:47:13 +0900

    Menu 구현

--- front/src/styles/components/layout/header.ts
+++ front/src/styles/components/layout/header.ts
@@ -1,15 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "styles/theme";
 
 export const HeaderContainer = styled.header`
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-
-	padding: 10px 5%;
+	height: 90px;
+	padding: 0 5%;
 `;
 
-export const NavBtn = styled.button`
+export const NavBtn = styled.button<{ $isMenuOpen?: boolean }>`
 	display: inline-flex;
 	justify-content: center;
 	align-items: center;
@@ -34,5 +34,41 @@ export const NavBtn = styled.button`
 
 	&:hover {
 		color: ${theme.color.lightPurple};
+	}
+
+	${({ $isMenuOpen }) =>
+		$isMenuOpen &&
+		css`
+			color: ${theme.color.lightPurple};
+		`}
+`;
+
+export const NavContainer = styled.nav<{ $isMenuOpen: boolean }>`
+	display: ${({ $isMenuOpen }) => ($isMenuOpen ? "block" : "none")};
+	position: fixed;
+	top: 90px;
+	bottom: 0;
+	right: 0;
+	width: 30vw;
+	background-color: ${theme.color.white};
+	color: ${theme.color.purple};
+	font-weight: 600;
+`;
+
+export const NavListItem = styled.li`
+	width: 100%;
+	font-size: ${theme.fontSize.md};
+	text-align: center;
+	cursor: pointer;
+
+	&:hover {
+		background-color: ${theme.color.lightPurple};
+		color: ${theme.color.white};
+	}
+
+	& > a {
+		display: block;
+		width: 100%;
+		padding: 5%;
 	}
 `;
