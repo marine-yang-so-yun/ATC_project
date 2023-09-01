ea95d849a4e85f0faf9ca9afbefad7b3186ff8a6
juhee kang <mnmn092631@gmail.com>
2023-09-01 09:37:16 +0900

    simulator style

--- front/src/styles/components/home/simulator.style.ts
+++ front/src/styles/components/home/simulator.style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 
 export const ThreeCanvas = styled(Canvas)`
-	width: 100vw;
+	margin: 0 auto;
+	width: 80vw !important;
 	height: 100vh !important;
 `;
