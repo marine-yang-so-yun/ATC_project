729f6e378429ad49bc46de6946a5065f0247cdf3
juhee kang <mnmn092631@gmail.com>
2023-09-14 15:38:34 +0900

    redux modal 생성

--- front/src/store/AppState.ts
+++ front/src/store/AppState.ts
@@ -1,7 +1,9 @@
 import * as Notice from "./notice";
 import * as BlockCrane from "./blockCrane";
+import * as Modal from "./modal";
 
 export type AppState = {
 	notices: Notice.State;
 	blockCrane: BlockCrane.State;
+	modal: Modal.State;
 };
--- front/src/store/rootReducer.ts
+++ front/src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import * as Notice from "./notice";
 import * as BlockCrane from "./blockCrane";
+import * as Modal from "./modal";
 
 export const rootReducer = combineReducers({
 	notices: Notice.reducer,
 	blockCrane: BlockCrane.reducer,
+	modal: Modal.reducer,
 });
