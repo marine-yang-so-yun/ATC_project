3af26f82b643202c80d459dd5e7808364d489862
juhee kang <mnmn092631@gmail.com>
2023-09-18 07:28:22 +0900

    공통 스타일 추출, style import 수정

--- front/src/styles/page/notice/noticeList.style.ts
+++ front/src/styles/page/notice/noticeList.style.ts
@@ -1,48 +1,5 @@
 import styled, { css } from "styled-components";
-import { MainTitle } from "styles/commons";
 import { theme } from "styles/theme";
-
-export const TitleContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	padding: 3%;
-	background-color: ${theme.color.purple};
-`;
-
-export const NoticeTitle = styled(MainTitle)`
-	color: ${theme.color.white};
-	margin-bottom: 0;
-`;
-
-export const CateBtn = styled.button<{ $active: boolean }>`
-	margin-top: 30px;
-	padding: 10px 15px;
-	background-color: ${theme.color.white};
-	font-weight: 600;
-	transition: all 250ms ease-in-out;
-
-	&:first-child {
-		border-radius: 10px 0 0 10px;
-	}
-
-	&:last-child {
-		border-radius: 0 10px 10px 0;
-	}
-
-	&:hover {
-		background-color: ${({ $active }) =>
-			$active ? `${theme.color.lightPurple}` : `${theme.color.lightGray}`};
-	}
-
-	${({ $active }) =>
-		$active &&
-		css`
-			background-color: ${theme.color.lightPurple};
-			color: ${theme.color.white};
-		`}
-`;
 
 export const NoticeOlContainer = styled.div`
 	padding: 1% 5%;
