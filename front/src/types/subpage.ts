0f02096d75d04993b2757d2b9b7ada187bfedc1a
juhee kang <mnmn092631@gmail.com>
2023-09-20 22:51:52 +0900

    csv 파일 다운로드 타입

---
+++ front/src/types/subpage.ts
@@ -0,0 +1,11 @@
+export interface ContainerWorkCSVData {
+	container: string;
+	workCode: string;
+	shipVoyage: string;
+	fromPosition: string;
+	toPosition: string;
+	timeEnd: Date;
+	fullOrEmpty: string;
+	containerSize: string;
+	crane: string;
+}
