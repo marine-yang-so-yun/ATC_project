8bba079e5d615009f138851f528e93a6e5d6457f
juhee kang <mnmn092631@gmail.com>
2023-09-21 15:36:15 +0900

    footer 스타일

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { SectionTitle } from "styles/commons";
 import * as S from "styles/components/layout/footer.style";
 
 const Footer = () => {
 	return (
 		<S.FooterContainer>
+			<SectionTitle>TOTAL SOFT BANK ATC MONITORING SYSTEM</SectionTitle>
+			<ul>
+				<li>
+					<Link to="/">법적고지</Link>
+				</li>
+				<li>
+					<Link to="/">개인정보취급방침</Link>
+				</li>
+			</ul>
 			<span>copyrightⓒ 2023 All rights reserved by ATC Project</span>
 		</S.FooterContainer>
 	);
--- front/src/styles/components/layout/footer.style.ts
+++ front/src/styles/components/layout/footer.style.ts
@@ -2,8 +2,19 @@ import styled from "styled-components";
 import { theme } from "styles/theme";
 
 export const FooterContainer = styled.footer`
-	padding: 5%;
+	display: flex;
+	flex-direction: column;
+	padding: 3% 5%;
 	background-color: ${theme.color.purple};
 	color: ${theme.color.white};
 	text-align: center;
+
+	& > ul {
+		margin: 0 auto 15px;
+		display: flex;
+
+		& > li + li {
+			margin-left: 15px;
+		}
+	}
 `;
